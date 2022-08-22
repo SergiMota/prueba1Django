1406bf8b433b010b22e1b6759376366800008657
SergiMota <sergitelematics@gmail.com>
2022-08-22 23:18:33 +0200

    anyadir y borrar elemntos

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,34 @@
-from django.http import HttpResponse
+from django.http import Http404,HttpResponseRedirect,HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
-data={
-    'movies':[
-        {
-            'id':5,
-            'title':'Jaws',
-            'year': '1976'
-        },
-        {
-            'id':6,
-            'title':'Terminator',
-            'year': '1991'
-        },
-        {
-            'id':7,
-            'title':'Megalodon',
-            'year': '1999'
-        }
-    ]
-}
 
 def movies(request):
-    return render(request, 'movies/movies.html',data )
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html',{'movies':data} )
 
 def home(request):
     return HttpResponse("Home page") 
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request,'movies/detail.html',{'movie':data})
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title,year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, 'movies/add.html')
+
+def delete(request,id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie does not exist')
+    movie.delete()
+    return HttpResponseRedirect('/movies')
